525009a68efd850d1ce9064b6f15d224420dfdb5
Francisco Ferreira <Franzx41@users.noreply.github.com>
2024-10-26 22:44:47 -0300

    Update analyser.py

--- analisador-sintatico/analyser.py
+++ analisador-sintatico/analyser.py
@@ -18,7 +18,7 @@ def main():
 		- Use parser.nome_da_producao_que_estou_trabalhando() para testar o seu código;
 
 	'''
-	TEST_FILE = './test/nome_da_producao_que_estou_trabalhando.txt' # Exemplo: ./test/function_sample.txt (não esquecer de remover antes antes de enviar para o repositório)
+	#TEST_FILE = './test/nome_do_arquivo_de_teste_que_estou_trabalhando.txt' # Exemplo: ./test/function_sample.txt (não esquecer de remover antes antes de enviar para o repositório)
 
 	tokens = lexical_analise(TEST_FILE)
 	if tokens :
