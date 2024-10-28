d19a92c26e26ce607a7114486560fab561a71fbe
Luis Fernando do Rosario Cintra <82674962+fernandocintra2871@users.noreply.github.com>
2024-10-28 18:41:08 -0300

    adicionando expression variables

--- analisador-sintatico/parser.py
+++ analisador-sintatico/parser.py
@@ -63,10 +63,41 @@ class Parser():
         #self.main() TODO Ainda não implementado
 
  #--------------------- Expressões aritméticas ---------------------   
-    def arithmetic_expression(self):
-        self.arithmetic_operating()
-        if self.lookahead()["lexeme"] in ['+', '-']:
-            self.arithmetic_sum()
+    # Caso: <primitive type> identifier ';' | identifier identifier ';' | <primitive type> identifier '=' <value> ';' | <primitive type> <vector position> ';'                                
+    def expression_declaration(self):
+        if self.lookahead()['category'] == 'IDENTIFIER':
+            self.match_category("IDENTIFIER")
+        else:
+            #self.primitive_type()
+            if self.lookahead()['category'] == 'IDENTIFIER':
+                self.match_category("IDENTIFIER")
+                if self.lookahead()['lexeme'] == '=':
+                    #self.value()
+                    pass
+            else:
+                #self.vector_position()
+                pass
+        self.match_lexeme([";"])
+            
+    # Caso: <expression declaration> <expression variables> | <expression declaration>
+    def expression_variables(self):
+        self.expression_declaration()
+        if self.lookahead()['category'] == 'IDENTIFIER' or self.lookahead()['lexeme'] in ['integer', 'float', 'boolean', 'string']:
+            self.expression_variables()
+       
+    
+    def variables(self):
+        self.match_lexeme('variables')
+        self.match_lexeme('{')
+        if self.lookahead()['category'] == 'IDENTIFIER' or self.lookahead()['lexeme'] in ['integer', 'float', 'boolean', 'string']:
+            self.expression_variables()
+        self.match_lexeme(['}'])
+
+
+     def arithmetic_expression(self):
+            self.arithmetic_operating()
+            if self.lookahead()["lexeme"] in ['+', '-']:
+                self.arithmetic_sum()
     
     def arithmetic_operating(self):
         self.arithmetic_value()
@@ -74,19 +105,19 @@ class Parser():
             self.arithmetic_multiplication()
 
     def arithmetic_value(self):
-        if self.lookahead()["category"] == "number":
+        if self.lookahead()["category"] == "NUMBER":
             self.match_category("number")
-        elif self.lookahead()["lexeme"] == "(":
-            self.match_lexeme("(")
-            self.arithmetic_expression()
-            self.match_lexeme(")")
-        elif self.lookahead()["category"] == "identifier":
+        elif self.lookahead()["category"] == "IDENTIFIER":
             if self.lookahead(1)["category"] == "(":
                 #self.function_call()
                 pass
             else:
-                self.attribute()
-               
+                #self.attribute()
+                pass
+        else:
+            self.match_lexeme("(")
+            self.arithmetic_expression()
+            self.match_lexeme(")")
 
     def arithmetic_sum(self):
         self.match_lexeme(['+', '-'])
