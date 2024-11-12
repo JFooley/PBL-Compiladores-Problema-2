36678ec827a34baf3d78679464de30a9a2db9f18
caleosilva <caleosilva75@gmail.com>
2024-11-12 20:10:32 -0300

    feat: tabelas

---
+++ analisador_semantico/tables.py
@@ -0,0 +1,35 @@
+class TabelaIdentificadores():
+    def __init__(self, nome, tipo, valor=None, tipoRetorno=None, parametros=None, tamanho=0):
+        self.nome = nome
+        self.tipo = tipo
+        self.valor = valor
+        self.tipoRetorno = tipoRetorno
+        self.parametros = parametros
+        self.tamanho = tamanho
+    
+    def __repr__(self):
+        pass
+    
+class TabelaDeclaracaoRegistradores():
+    def __init__(self, nome, atributos):
+        self.nome = nome
+        self.atributos = atributos # {“nome atributo”: {tipo: “...”, “tamanho”: “...”} }
+
+    def __repr__(self):
+            pass
+
+class TabelaPares():
+    def __init__(self):
+         self.tabela = []
+         
+    def adicionarPar(self, pai, tabelaIdentificadores):
+        novoPar = {
+            "pai": pai,
+            "tabela": tabelaIdentificadores 
+        }
+        
+        self.tabela.append(novoPar)
+    
+    def __repr__(self):
+            pass
+        
