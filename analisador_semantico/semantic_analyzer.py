65f30149ef4b39a00f8b2d61f26935868c41ee37
Luis Fernando do Rosario Cintra <82674962+fernandocintra2871@users.noreply.github.com>
2024-11-22 00:25:12 -0300

    Adicionado função de validar os parametros
    ainda incompleta

--- analisador_semantico/semantic_analyzer.py
+++ analisador_semantico/semantic_analyzer.py
@@ -31,3 +31,24 @@ class SemanticAnalyzer:
 
     def get_error_list(self):
         return self.error_list
+    
+    # Validar parametro (luis)
+    def func_parameter_validator(token_list):
+        parameter_list = []
+        for token in token_list:
+            if token['category'] == 'IDENTIFIER':
+                parameter_list.append(token)
+        function_name = parameter_list.pop(0)
+        function_name = function_name['lexeme']
+    
+        parameters_types = self.symbol_map['global'][function_name]['parameters']
+    
+        n = 0
+        while n  < len(parameter_list):
+            if parameter_list[0]['category'] == 'IDENTIFIER':
+                parameter_type = self.symbol_map[self.scope][parameter['lexeme']]['type']
+                if parameter_type != parameters_types[n]:
+                    self.error_list.append("parametro invalido") #falta deixar bonitinho ainda
+            # demais casos, numeros, strings...
+    
+            n += 1
