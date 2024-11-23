dcbced7cc3b11ce8a54e302099b86a508d47d20a
João Gabriel <gabriel.lima.almeida@gmail.com>
2024-11-22 22:24:21 -0300

    fix: find table function

--- analisador_semantico/semantic_analyzer.py
+++ analisador_semantico/semantic_analyzer.py
@@ -13,9 +13,6 @@ class SemanticAnalyzer:
         # Lista de erros semânticos
         self.error_list = []
 
-        # Index do scopo atual
-        self.current_scope_index = 0
-
         self.tokens = []
 
     ################################ Funções auxiliares ################################
@@ -23,35 +20,44 @@ class SemanticAnalyzer:
         return self.error_list
     
     ## Gera um erro na lista de erros ## 
-    def throw_erro(self, message, token):
+    def throw_error(self, message, token):
         self.error_list.append({"position": token["line"], "message": message})
 
-    ## Busca uma entrada específica na tabela designada ## 
-    def find_variable_entry(self, target_table_index, token, throw_erro = True):
+    ## Busca uma entrada específica na cadeia de tabelas designada ## 
+    def find_table_entry(self, target_table_index, token, throw_erro = True):
+        selected_entry = None
+
+        # Busca no escopo atual
         for entry in self.pairs_table.tabela[target_table_index]["tabela"]:
             if entry.nome == token["lexeme"]:
-                return entry
+                selected_entry = entry
+
+        # Busca recursivamente no escopo pai até chegar na global, caso não tenha achado
+        if selected_entry == None and target_table_index > 0:
+            selected_entry = self.find_table_entry(self.pairs_table.tabela[target_table_index]["pai"], token, throw_erro)
         
-        if (throw_erro): self.throw_erro(f"{token["lexeme"]} não existe nesse escopo.", token)
-        return None
+        # Causa erro se necessário
+        if (throw_erro and selected_entry == None): self.throw_error(f"{token["lexeme"]} não existe nesse escopo.", token)
+
+        return selected_entry
 
     ################################ Funções de erro ################################
     def wrong_type_assign(self, current_table_index, variable, value):
         # Caso valor = variável
         if value["category"] == "IDENTIFIER":
-            variable_entry: EntryIdentificadores = self.find_variable_entry(current_table_index, variable["lexeme"])
-            value_entry: EntryIdentificadores = self.find_variable_entry(current_table_index, value["lexeme"])
+            variable_entry: EntryIdentificadores = self.find_table_entry(current_table_index, variable["lexeme"])
+            value_entry: EntryIdentificadores = self.find_table_entry(current_table_index, value["lexeme"])
 
             if (variable_entry == None or value_entry == None):
                 return False
             
             else:
                 if (variable_entry.tipo != value_entry.tipo):
-                    self.throw_erro(f"{value_entry.tipo} não pode ser convertido em {variable_entry.tipo}.", value)
+                    self.throw_error(f"{value_entry.tipo} não pode ser convertido em {variable_entry.tipo}.", value)
 
         # Caso valor = literal
         else:
-            variable_entry: EntryIdentificadores = self.find_variable_entry(current_table_index, variable["lexeme"])
+            variable_entry: EntryIdentificadores = self.find_table_entry(current_table_index, variable["lexeme"])
             
             if (variable_entry == None):
                 return False
@@ -59,26 +65,26 @@ class SemanticAnalyzer:
                 match value["category"]:
                     case "NUMBER":
                         if (variable_entry.tipo != "float" and variable_entry.tipo != "integer" and ("." in value["lexeme"] and variable_entry.tipo == "integer")):
-                            self.throw_erro(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
+                            self.throw_error(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
                     
                     case "STRING":
                         if (variable_entry.tipo != "string"):
-                            self.throw_erro(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
+                            self.throw_error(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
 
                     case "CHARACTER":
                         if (variable_entry.tipo != "character"):
-                            self.throw_erro(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
+                            self.throw_error(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
 
                     case "BOOLEAN":
                         if (variable_entry.tipo != "boolean"):
-                            self.throw_erro(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
+                            self.throw_error(f"{value["category"]} não pode ser convertido em {variable_entry.tipo}.", value)
         return True
     
     def repeated_statement(self, current_table_index, new_variable):
-        new_variable_entry: EntryIdentificadores = self.find_variable_entry(current_table_index, new_variable["lexeme"], throw_erro= False)
+        new_variable_entry: EntryIdentificadores = self.find_table_entry(current_table_index, new_variable["lexeme"], throw_erro= False)
         
         if (new_variable_entry != None):
-            self.throw_erro(f"{new_variable["lexeme"]} já existe neste escopo.", new_variable)
+            self.throw_error(f"{new_variable["lexeme"]} já existe neste escopo.", new_variable)
             return False
         else:
             return True
