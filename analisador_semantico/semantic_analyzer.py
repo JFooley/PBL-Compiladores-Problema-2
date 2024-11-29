a2a229e5cb0e223dcd8f948dcddcb40873a1aa52
francisco <francisco.a.ferreira@outlook.com>
2024-11-29 06:14:02 -0300

    Added: validação de acesso a registro atributo

--- analisador_semantico/semantic_analyzer.py
+++ analisador_semantico/semantic_analyzer.py
@@ -261,8 +261,8 @@ class SemanticAnalyzer:
             register_entry = EntryRegisters(nome["lexeme"], {atributo["lexeme"]: {"tipo": tipo["lexeme"]}})
             temp.append(register_entry)
         
-        if(size_error == len(self.error_list)):
-            self.registers_type_table.append(temp)
+        if (size_error == len(self.error_list)):
+            self.registers_type_table.extend(temp)
 
     '''deve enviar a lista de tokens assim: Cadastro pessoa; ou Cadastro pessoa = pessoa1;
       Para o caso de Cadastro pessoa = pessoa1 precisa criar outra função para verificar o pessoa1  
@@ -310,7 +310,8 @@ class SemanticAnalyzer:
         vector_length = 0
         for i in range(0, len(token_list)):
             token = token_list[i]
-            if token['category'] == 'KEYWORD':  #obs: o tipo pode ser tbm identifier, que é o caso de ser um register
+
+            if token['category'] == 'KEYWORD' or self.get_registers(token):  #obs: o tipo pode ser tbm identifier, que é o caso de ser um register
                 variable_type = token['lexeme']
             
             elif token['category'] == 'IDENTIFIER':
@@ -511,3 +512,41 @@ class SemanticAnalyzer:
         #Verificar erro se a função existe
         #Verificar erro dos parametros
         return
+
+    def util_is_attribute(self, reg_entry, attr_key):
+        '''
+        Função auxiliar para checar se um atributo existe em uma entrada da tabela de registro
+        '''
+        for e in reg_entry:
+            if (attr_key in e.atributos): return True
+        return False
+
+    def util_is_register_access(self, prev_token, post_token):
+        '''
+        Função auxiliar usada para determinar se um
+        '''
+        entry = self.find_table_entry(self.current_table_index, prev_token)
+        if (entry):
+            reg_entry = self.get_registers(dict(lexeme=entry.tipo))
+            return bool(reg_entry) and bool(self.util_is_attribute(reg_entry, post_token['lexeme']))
+        return False
+
+    def validate_is_register_access(self, token_list):
+        '''
+        Verifica se a list de tokens acumaladas são usadas como parâmetros de accesso de um registro declarado. Usar essa função dentro de parser#register_position() e parser#register_access()
+        É esperedo uma token_list do tipo: [TOKEN_IDENTIFIER, TOKEN_DOT, TOKEN_IDENTIFIER_1, ..., TOKEN_DOT, TOKEN_IDENTIFIER_N] -> identificador_register.identificador_atributo_1,.., .identificador_atributo_N
+        '''
+
+        is_analysis_ok = False
+        try:
+            for i, curr_token in enumerate(token_list):
+                if "." in curr_token['lexeme']: 
+                    prev_token = token_list[i - 1]
+                    post_token = token_list[i + 1]
+                    
+                    is_analysis_ok = self.util_is_register_access(prev_token, post_token)
+        except:
+            is_analysis_ok = False
+
+        if not is_analysis_ok:
+            self.throw_error(f"Erro: Os tokens '{token_list}' não são tokens de acesso a um atributo de registro.", token_list[0])        
