f2d91cd931d1be3183c0b5b9c8d8b680e8407287
Lucas Gabriel <68016822+lucasxgb@users.noreply.github.com>
2024-12-06 11:08:04 -0300

    Update semantic_analyzer.py

--- analisador_semantico/semantic_analyzer.py
+++ analisador_semantico/semantic_analyzer.py
@@ -774,7 +774,7 @@ class SemanticAnalyzer:
         value_list = []
         # Verificar se só tem apenas o return na lista
         if len(token_list) == 1:
-            if self.last_function_type != None self.last_function_type['lexeme'].lower() != "empty":
+            if self.last_function_type != None and self.last_function_type['lexeme'].lower() != "empty":
                 self.throw_error("O retorno da função está vazio", token_list[0])
                 return
             else:
